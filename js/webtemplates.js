a7f1c41260e0c137500860c9233d870e6348e5f8
Kenneth C Mendenhall II <mendenhall_kenneth1@nulytixcloud.com>
2024-03-24 07:35:09 -0400

    Initial Commit
    v0.5 of ASC Real Estate Leeward Website

---
+++ js/webtemplates.js
@@ -0,0 +1,5 @@
+/*
+    Template router for standardized branding across all ASC Real Estate Webpages
+*/
+let y = new Date().getFullYear();
+document.body.innerHTML = "<p>&copy; 2023 - " + y + " | Atlantic South Commercial Real Estate | </p>";
